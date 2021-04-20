67e36314944e67d5cd8db765fac6dcc53ebd6487
Welison <welison.paixao-ext@viavarejo.com.br>
2021-04-20 10:38:19 -0300

    definindo tabelas pessoa e postagem

--- backend/src/main/java/usjt/project/civitas/civitas/entity/Pessoa.java
+++ backend/src/main/java/usjt/project/civitas/civitas/entity/Pessoa.java
@@ -1,4 +1,10 @@
 package usjt.project.civitas.civitas.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "pessoa")
 public class Pessoa {
+
 }
--- backend/src/main/java/usjt/project/civitas/civitas/entity/Postagem.java
+++ backend/src/main/java/usjt/project/civitas/civitas/entity/Postagem.java
@@ -1,4 +1,9 @@
 package usjt.project.civitas.civitas.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "postagem")
 public class Postagem {
 }
