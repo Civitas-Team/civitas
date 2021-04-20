ae81110145cf8632a041034c95d5a2498ed32bfb
Welison <welison.paixao-ext@viavarejo.com.br>
2021-04-20 11:01:10 -0300

    ---

--- backend/src/main/java/usjt/project/civitas/civitas/entity/Pessoa.java
+++ backend/src/main/java/usjt/project/civitas/civitas/entity/Pessoa.java
@@ -1,10 +1,13 @@
 package usjt.project.civitas.civitas.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name= "pessoa")
+@Table(name = "pessoa")
 public class Pessoa {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
 }
--- backend/src/main/java/usjt/project/civitas/civitas/entity/Postagem.java
+++ backend/src/main/java/usjt/project/civitas/civitas/entity/Postagem.java
@@ -1,9 +1,12 @@
 package usjt.project.civitas.civitas.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name= "postagem")
 public class Postagem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 }
