f684bb7353da39dde80d0a1cc94b784c1b9fe9c7
Beatriz Rodrigues <beatriz07012001@gmail.com>
2021-05-22 16:25:33 -0300

    Tela de login do usuário

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -8,10 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   isCarregando: boolean = false;
-
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
