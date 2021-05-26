aed98d68a6f98869f60f6e0948ec57d21bea1d86
vilt.luizb.utic <luiz.berber@vilt-group.com>
2021-05-25 23:44:01 -0300

    WIP

--- backend/src/main/java/usjt/project/civitas/civitas/controller/FiltroControleAcesso.java
+++ backend/src/main/java/usjt/project/civitas/civitas/controller/FiltroControleAcesso.java
@@ -35,6 +35,8 @@ public class FiltroControleAcesso implements Filter {
 		HttpServletRequest req = (HttpServletRequest) request;
 
 		res.setHeader("Allowed-Origin-Patterns", "*");
+//		res.setHeader("Access-Control-Allow-Origin", "*");
+		res.setHeader("Access-Control-Allow-Credentials", "false");
 		res.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
 		res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
